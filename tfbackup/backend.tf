95e1ec63ed13f1db4df2c143a2400c7e0bea8d2f
foulscar <corbingrossen@gmail.com>
2024-05-21 16:57:18 -0400

    update tf

--- tfbackup/backend.tf
+++
@@ -1,9 +0,0 @@
-module "remote_state" {
-  source        = "nozaq/remote-state-s3-backend/aws"
-  kms_key_alias = "tf-state-key"
-
-  providers = {
-    aws         = aws
-    aws.replica = aws.replica
-  }
-}
