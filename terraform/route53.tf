f4976a1078d8e5672cf6b41e80f84127c7adde6c
foulscar <corbingrossen@gmail.com>
2024-05-21 18:54:55 -0400

    update tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,14 +2,6 @@ resource "aws_route53_zone" "main" {
   name = "bta.corbinpersonal.me"
 }
 
-resource "aws_route53_record" "ns" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "bta.corbinpersonal.me"
-  type    = "NS"
-  ttl     = "30"
-  records = aws_route53_zone.main.name_servers
-}
-
 resource "aws_route53_record" "server_cname" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "play.bta.corbinpersonal.me"
