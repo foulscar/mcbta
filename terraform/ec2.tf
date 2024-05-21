03d13aab94e9b34cca806c50e7e3373453081e48
foulscar <corbingrossen@gmail.com>
2024-05-21 17:14:57 -0400

    update tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -15,4 +15,41 @@ resource "aws_key_pair" "bta" {
   public_key = file("bta_public.pem")
 }
 
+resource "aws_security_group" "bta" {
+  name        = "bta"
+  description = "bta"
 
+  ingress = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 25565
+      to_port     = 25565
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 19132
+      to_port     = 19132
+      protocol    = "udp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.bta.id
+  network_interface_id = aws_instance.bta.primary_network_interface_id
+}
