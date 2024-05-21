7cd5ad42784b7d82ec527d856cd3410acf3f1aaa
foulscar <corbingrossen@gmail.com>
2024-05-21 16:14:31 -0400

    update tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,9 +1,18 @@
 resource "aws_instance" "bta" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
-
+  subnet_id = aws_subnet.main_public.id
+  key_name = aws_key_pair.bta.key_name
+  associate_public_ip_address = true
 
   tags = {
     Name = "bta"
   }
 }
+
+resource "aws_key_pair" "bta" {
+  key_name = "bta.pem"
+  public_key = file("bta_public.pem")
+}
+
+
