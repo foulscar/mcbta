2d1fda491338a105c0f8b17a1878e23bb62c2171
foulscar <corbingrossen@gmail.com>
2024-05-21 16:36:14 -0400

    update tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,7 @@
-module "terraform_state_backend" {
-  source = "cloudposse/tfstate-backend/aws"
-  version     = "1.4.1"
-  namespace  = "tf"
-  stage      = "prod"
-  name       = "terraform"
-  attributes = ["state"]
+module "remote_state" {
+  source = "nozaq/remote-state-s3-backend/aws"
 
-  terraform_backend_config_file_path = "."
-  terraform_backend_config_file_name = "backend.tf"
-  force_destroy                      = false
+  providers = {
+    aws         = aws
+  }
 }
