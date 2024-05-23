976076dc3fb4267af2967be492338fffe476170c
foulscar <corbingrossen@gmail.com>
2024-05-23 16:20:50 -0400

    add panel cert

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -11,3 +11,17 @@ resource "aws_acm_certificate_validation" "bta_api" {
   certificate_arn         = aws_acm_certificate.bta_api.arn
   validation_record_fqdns = [for record in aws_route53_record.bta_api_cert_record : record.fqdn]
 }
+
+resource "aws_acm_certificate" "bta_panel" {
+  domain_name       = "panel.bta.corbinpersonal.me"
+  validation_method = "DNS"
+
+  tags = {
+    Name = "bta-panel-cert"
+  }
+}
+
+resource "aws_acm_certificate_validation" "bta_panel" {
+  certificate_arn         = aws_acm_certificate.bta_panel.arn
+  validation_record_fqdns = [for record in aws_route53_record.bta_panel_cert_record : record.fqdn]
+}
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -26,3 +26,20 @@ resource "aws_route53_record" "bta_api_cert_record" {
   type            = each.value.type
   zone_id         = aws_route53_zone.main.zone_id
 }
+
+resource "aws_route53_record" "bta_panel_cert_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.bta_panel.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
+}
