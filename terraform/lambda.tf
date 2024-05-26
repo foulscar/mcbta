df2cd0d5fdd027339fbf69160a23d3b681b70344
foulscar <corbingrossen@gmail.com>
2024-05-26 13:31:32 -0400

    add auth lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,18 +1,3 @@
-provider "random" {}
-
-resource "random_string" "trigger" {
-  length  = 8
-  special = false
-}
-
-data "archive_file" "btaAPIOperate" {
-  type        = "zip"
-  source_dir  = "${path.module}/../lambdas/btaAPIOperate"
-  output_path = "${path.module}/../lambdas/btaAPIOperate.zip"
-
-  depends_on = [random_string.trigger]
-}
-
 resource "aws_lambda_function" "btaAPIOperate" {
   filename      = data.archive_file.btaAPIOperate.output_path
   function_name = "btaAPIOperate"
@@ -26,14 +11,6 @@ resource "aws_lambda_function" "btaAPIOperate" {
       INSTANCE_ID = aws_instance.bta.id
     }
   }
-}
-
-data "archive_file" "btaAPIAuthAdmin" {
-  type        = "zip"
-  source_dir  = "${path.module}/../lambdas/btaAPIAuthAdmin"
-  output_path = "${path.module}/../lambdas/btaAPIAuthAdmin.zip"
-
-  depends_on = [random_string.trigger]
 }
 
 resource "aws_lambda_function" "btaAPIAuthAdmin" {
