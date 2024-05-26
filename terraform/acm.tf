fc8fd22f679bd0bbfee08e6c9167edb82be8e57d
foulscar <corbingrossen@gmail.com>
2024-05-26 12:40:37 -0400

    add lambda

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -12,16 +12,16 @@ resource "aws_acm_certificate_validation" "bta_api" {
   validation_record_fqdns = [for record in aws_route53_record.bta_api_cert_record : record.fqdn]
 }
 
-resource "aws_acm_certificate" "bta_panel" {
-  domain_name       = "panel.bta.corbinpersonal.me"
-  validation_method = "DNS"
-
-  tags = {
-    Name = "bta-panel-cert"
-  }
-}
-
-resource "aws_acm_certificate_validation" "bta_panel" {
-  certificate_arn         = aws_acm_certificate.bta_panel.arn
-  validation_record_fqdns = [for record in aws_route53_record.bta_panel_cert_record : record.fqdn]
-}
+#resource "aws_acm_certificate" "bta_panel" {
+#  domain_name       = "panel.bta.corbinpersonal.me"
+#  validation_method = "DNS"
+#
+#  tags = {
+#    Name = "bta-panel-cert"
+#  }
+#}
+#
+#resource "aws_acm_certificate_validation" "bta_panel" {
+#  certificate_arn         = aws_acm_certificate.bta_panel.arn
+#  validation_record_fqdns = [for record in aws_route53_record.bta_panel_cert_record : record.fqdn]
+#}
