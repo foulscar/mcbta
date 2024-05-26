c83389472de562b3f28028db2a50b187622648ce
foulscar <corbingrossen@gmail.com>
2024-05-26 14:22:06 -0400

    add authorizer to api

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -32,6 +32,6 @@ resource "aws_apigatewayv2_integration" "btaOperate" {
 resource "aws_apigatewayv2_route" "btaOperate" {
   api_id = aws_apigatewayv2_api.bta.id
   route_key = "POST /operate"
-  target = aws_apigatewayv2_integration.btaOperate.integration_uri
+  target = "integrations/${aws_apigatewayv2_integration.btaOperate.id}"
   authorizer_id = aws_apigatewayv2_authorizer.btaAdmin.id
 }
