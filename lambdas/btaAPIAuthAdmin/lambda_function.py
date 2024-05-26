991ed3ce737a9686808247060ed60eb3372316e8
foulscar <corbingrossen@gmail.com>
2024-05-26 15:50:59 -0400

    add authorizer to api

--- lambdas/btaAPIAuthAdmin/lambda_function.py
+++ lambdas/btaAPIAuthAdmin/lambda_function.py
@@ -8,52 +8,26 @@ def lambda_handler(event, context):
     response = None
     if secretID is None:
         print("Failed to get SECRET_ID")
-        response = generatePolicy('user', 'DENY', event['methodArn'])
-        return response
+        return False
 
     client = boto3.client("secretsmanager")
     totpSecretKey = client.get_secret_value(SecretId=secretID)["SecretString"]
     if totpSecretKey is None:
         print("Failed to get totpSecretKey")
-        response = generatePolicy('user', 'DENY', event['methodArn'])
-        return response
+        return False
 
     totp = pyotp.TOTP(totpSecretKey)
 
     totpClientToken = event.get("authorizationToken")
     if totpClientToken is None:
         print("Failed to get totpClientToken")
-        response = generatePolicy('user', 'DENY', event['methodArn'])
-        return response
+        return False
 
     try:
         if totp.verify(totpClientToken):
-            response = generatePolicy('user', 'Allow', event['methodArn'])
+            return True
         else:
-            response = generatePolicy('user', 'DENY', event['methodArn'])
+            return False
     except Exception as e:
         print("Error verifying OTP token:", e)
-        response = generatePolicy('user', 'DENY', event.get('methodArn', ''))
-
-    try:
-        return json.loads(response)
-    except BaseException:
-        print("error")
-        return 'unauthorized'
-
-
-def generatePolicy(principalId, effect, resource):
-    authResponse = {}
-    authResponse['principalId'] = principalId
-    if (effect and resource):
-        policyDocument = {}
-        policyDocument['Version'] = '2012-10-17'
-        policyDocument['Statement'] = []
-        statementOne = {}
-        statementOne['Action'] = 'execute-api:Invoke'
-        statementOne['Effect'] = effect
-        statementOne['Resource'] = resource
-        policyDocument['Statement'] = [statementOne]
-        authResponse['policyDocument'] = policyDocument
-    authResponse_JSON = json.dumps(authResponse)
-    return authResponse_JSON
+        return False
--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -4,47 +4,48 @@ resource "aws_apigatewayv2_api" "bta" {
 }
 
 resource "aws_apigatewayv2_domain_name" "bta" {
-  domain_name     = "api.bta.corbinpersonal.me"
+  domain_name = "api.bta.corbinpersonal.me"
   domain_name_configuration {
     certificate_arn = aws_acm_certificate.bta_api.arn
-    endpoint_type = "REGIONAL"
+    endpoint_type   = "REGIONAL"
     security_policy = "TLS_1_2"
   }
 }
 
 resource "aws_apigatewayv2_stage" "bta_main" {
-  api_id = aws_apigatewayv2_api.bta.id
-  name = "main"
+  api_id      = aws_apigatewayv2_api.bta.id
+  name        = "main"
   auto_deploy = true
 }
 
 resource "aws_apigatewayv2_api_mapping" "bta" {
-  api_id = aws_apigatewayv2_api.bta.id
+  api_id      = aws_apigatewayv2_api.bta.id
   domain_name = aws_apigatewayv2_domain_name.bta.domain_name
-  stage = "main"
+  stage       = "main"
 
-  depends_on = [ aws_apigatewayv2_stage.bta_main ]
+  depends_on = [aws_apigatewayv2_stage.bta_main]
 }
 
 resource "aws_apigatewayv2_authorizer" "btaAdmin" {
-  api_id = aws_apigatewayv2_api.bta.id
-  authorizer_type = "REQUEST"
-  name = "btaAdmin"
-  authorizer_uri = aws_lambda_function.btaAPIAuthAdmin.invoke_arn
+  api_id                            = aws_apigatewayv2_api.bta.id
+  authorizer_type                   = "REQUEST"
+  enable_simple_responses           = true
+  name                              = "btaAdmin"
+  authorizer_uri                    = aws_lambda_function.btaAPIAuthAdmin.invoke_arn
   authorizer_payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_integration" "btaOperate" {
-  api_id = aws_apigatewayv2_api.bta.id
-  integration_type = "AWS_PROXY"
+  api_id             = aws_apigatewayv2_api.bta.id
+  integration_type   = "AWS_PROXY"
   integration_method = "POST"
-  integration_uri = aws_lambda_function.btaAPIOperate.invoke_arn
+  integration_uri    = aws_lambda_function.btaAPIOperate.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "btaOperate" {
-  api_id = aws_apigatewayv2_api.bta.id
-  route_key = "POST /operate"
-  target = "integrations/${aws_apigatewayv2_integration.btaOperate.id}"
+  api_id             = aws_apigatewayv2_api.bta.id
+  route_key          = "POST /operate"
+  target             = "integrations/${aws_apigatewayv2_integration.btaOperate.id}"
   authorization_type = "CUSTOM"
-  authorizer_id = aws_apigatewayv2_authorizer.btaAdmin.id
+  authorizer_id      = aws_apigatewayv2_authorizer.btaAdmin.id
 }
